8883e210f4ceb6592c8f24e542035bacd2cc1135
Kasey McCormick <mccormick.kasey@gmail.com>
2015-08-13 15:45:16 -0500

    footer finished

---
+++ global.js
@@ -0,0 +1,14 @@
+function bigImg(x) {
+    x.style.height = "240px";
+    x.style.width = "240px";
+}
+
+function normalImg(x) {
+    x.style.height = "95px";
+    x.style.width = "95px";
+    showBio;
+}
+
+function showBio(){
+  document.elementById("bio").
+}
