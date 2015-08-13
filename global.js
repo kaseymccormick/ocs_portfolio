abf5b812a082d1557a2dd564e89d4d481979182a
Kasey McCormick <mccormick.kasey@gmail.com>
2015-08-13 17:10:34 -0500

    pictures change size on hover

--- global.js
+++ global.js
@@ -1,11 +1,39 @@
-function bigImg(x) {
-    x.style.height = "240px";
-    x.style.width = "240px";
-}
+// //w3c schools
+// var picture = document.getElementsByClassName("class__pictures__classmate__image");
+//onmouseover="bigImg(this)" onmouseout="normalImg(this)"
+// function bigImg() {
+//   picture.style.height = "240px";
+//   picture.style.width = "240px";
+// }
+//
+// function normalImg() {
+//   picture.style.height = "95px";
+//   picture.style.width = "95px";
+// }
+//
+// normalImg(picture);
+//
+// // test below
+// var yourImg = document.getElementById('yourImgId');
+// {
+//     yourImg.style.height = '40px';
+//     yourImg.style.width = '40px';
+// }
 
-function normalImg(x) {
-    x.style.height = "95px";
-    x.style.width = "95px";
-    showBio;
-}
 
+//
+// function largeImg(){
+//   document.getElementsByClassName("class__pictures__classmate__image").addEventListener("mouseover", function(){
+//     x.style.height = "240px";
+//     x.style.width = "240px";
+//   })
+// }
+//
+// function normalImg(){
+//   document.getElementsByClassName("class__pictures__classmate__image").addEventListener("mouseout", function(){
+//     x.style.height = "95px";
+//     x.style.width = "95px";
+//   })
+//
+// }
+// normalImg();
